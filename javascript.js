205253cecd983d65e78045aa9d879ee07a2d2e01
Cameron Brown <moontsukuyomi6@gmail.com>
2025-05-16 21:50:08 +0200

    Add start game button and fix reset function not working properly

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const gameboard = (function () {
   let playerXTurn = true;
-  const gameboardArray = [
+  let gameboardArray = [
     '', '', '',
     '', '', '',
     '', '', ''
@@ -9,13 +9,17 @@ const gameboard = (function () {
     const boardCell = document.querySelectorAll('.board-cell');
     boardCell.forEach((cell) => {
       cell.addEventListener('click', () => {
+        const gameActive = play.isGameActive();
         const index = cell.dataset.index;
-        if (playerXTurn && gameboard.gameboardArray[index] === '') {
+        if (gameActive && playerXTurn && gameboard.gameboardArray[index] === '') {
           gameboard.gameboardArray[index] = 'X';
+          console.log(gameboardArray);
+
           playerXTurn = false;
           play.checkWin();
-        } else if (!playerXTurn && gameboard.gameboardArray[index] === '') {
+        } else if (gameActive && !playerXTurn && gameboard.gameboardArray[index] === '') {
           gameboard.gameboardArray[index] = 'O';
+          console.log(gameboardArray);
           playerXTurn = true;
           play.checkWin();
         }
@@ -31,9 +35,26 @@ function createPlayer(playerName, marker) {
 }
 
 const play = (function () {
+  const gameboardArray = gameboard.gameboardArray;
+  const resultElement = document.querySelector('.result');
+  let gameActive;
+  const startButton = document.querySelector('.start-button');
+  startButton.addEventListener('click', () => {
+    gameActive = true;
+    startButton.textContent = 'Restart'
+    resultElement.textContent = 'Game Start!';
+    if (startButton.textContent === 'Restart') {
+      startButton.addEventListener('click', () => {
+        display.reset();
+      })
+    }
+  });
+  if (!gameActive) {
+    resultElement.textContent = 'Press Start!'
+  }
+
   const playerX = createPlayer('Cameron', 'X');
   const playerO = createPlayer('Brandon', 'O');
-  const gameboardArray = gameboard.gameboardArray;
   const winCondition = [
     [0, 1, 2],
     [3, 4, 5],
@@ -45,21 +66,27 @@ const play = (function () {
     [2, 4, 6]
   ];
   const checkWin = function () {
-    const resultElement = document.querySelector('.result');
     const allExist = gameboardArray.every(cell => cell !== '');
     for (const [a, b, c] of winCondition)
       if (gameboardArray[a] && gameboardArray[a] === gameboardArray[b] && gameboardArray[a] === gameboardArray[c]) {
         if (gameboardArray[a] === 'X') {
           resultElement.textContent = `Player ${playerX.marker}: ${playerX.playerName}  Wins`;
+          gameActive = false;
         } else if (gameboardArray[a] === 'O') {
           resultElement.textContent = `Player ${playerO.marker}: ${playerO.playerName}  Wins`;
+          gameActive = false;
         }
       }
     if (allExist) {
       return 'Tie'
     }
   }
-  return { checkWin, playerX, playerO }
+  const isGameActive = function () {
+    if (gameActive) {
+      return gameActive;
+    }
+  }
+  return { checkWin, playerX, playerO, isGameActive }
 })();
 
 const display = (function () {
@@ -77,15 +104,12 @@ const display = (function () {
   }
 
   const reset = function () {
-    gameboard.gameboardArray = [
-      '', '', '',
-      '', '', '',
-      '', '', ''
-    ];
+    for (i = 0; i < gameboard.gameboardArray.length; i++) {
+      gameboard.gameboardArray[i] = '';
+    }
     renderGameboard();
   }
   return { renderGameboard, reset }
 })();
 
 display.renderGameboard();
-gameboard.addPlayerMarker();
